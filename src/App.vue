fadccfd6ceb19ef8824a52b9214334e6f9e41c03
nadiaanakampun <nadiaanakampun@student.uir.ac.id>
2024-06-20 18:27:29 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Menggunakan Store</h1>
+    <p>Akses count: {{ countStore.count }}</p>
+    <p>Akses doubleCount: {{ countStore.doubleCount }}</p>
+    <button @click="countStore.increment">+</button><br /><br />
+
+    <h1>Tanpa Store</h1>
+    <p>Akses count tanpa store: {{ count }}</p>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+import { useCountStore } from './stores/countStore'
+
+export default {
+  setup() {
+    const countStore = useCountStore()
+
+    function increment() {
+      countStore.increment()
+    }
+
+    return { countStore, increment }
+  }
+}
+</script>
